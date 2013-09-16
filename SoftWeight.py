89d96616cf6bf1523fbee97b35a4f0a0ca52303b
wihoho <wihoho@gmail.com>
2013-09-16 20:58:37 +0800

    Soft Weight Comparison
    original Method
    meanAP: 0.429429823493
    standard deviation: 0.0291790233439
    ----------------------------------------------------------
    Histograms with soft weight
    meanAP: 0.435854055036
    standard deviation: 0.0269956975705

--- SoftWeight.py
+++ SoftWeight.py
@@ -1,8 +1,53 @@
 __author__ = 'GongLi'
 
+from scipy.io import loadmat
+import SVM_T
+import numpy as np
+
+def evaluate(distance):
+
+    distances = []
+    distances.append(distance)
+    labels = loadmat("labels.mat")['labels']
+
+    all_aps = []
+    for i in range(1,6,1):
+        trainingIndices = loadmat(str(i)+".mat")['training_ind']
+        trainingIndiceList = []
+        testingIndices = loadmat(str(i)+".mat")['test_ind']
+        testingIndiceList = []
+
+        # Construct indices
+        for i in range(trainingIndices.shape[0]):
+            trainingIndiceList.append(trainingIndices[i][0] - 1)
+
+        for i in range(testingIndices.shape[1]):
+            testingIndiceList.append(testingIndices[0][i] - 1)
+
+        targetTrainingIndices = []
+        auxiliaryTrainingIndices = []
+        for i in trainingIndiceList:
+            if i <= 194:
+                targetTrainingIndices.append(i)
+            else:
+                auxiliaryTrainingIndices.append(i)
+
+        aps = SVM_T.runSVM_T(distances, labels, targetTrainingIndices, testingIndiceList)
+        all_aps.append(aps)
+
+    all_aps = np.array(all_aps)
+    meanAP = np.mean(all_aps)
+
+    rowMean = np.mean(all_aps, axis=1)
+    sd = np.std(rowMean)
+
+    print "meanAP: "+str(meanAP)
+    print "standard deviation: "+str(sd)
+
+
+
 
 if __name__ == "__main__":
-    import SVM_T
     import Utility as util
 
     originalKodakDistances = util.loadObject("/Users/GongLi/PycharmProjects/DomainAdaption/LevelZero/Kodak_distanceMatrix_version2.pkl")
@@ -11,12 +56,11 @@ if __name__ == "__main__":
     softWeightKodakDistance = util.loadObject("/Users/GongLi/PycharmProjects/DomainAdaption/NewMethods/KodakSoftWeight_DistanceMatrix_Level0.pkl")
     softWeightLabels = util.loadObject("/Users/GongLi/PycharmProjects/DomainAdaption/NewMethods/KodakSoftWeight_labels_Level0.pkl")
 
-    # Check whether these two labels are the same
-    if originalLabels == softWeightLabels:
-        print "Labels are the same"
+    evaluate(originalKodakDistances)
+    evaluate(softWeightKodakDistance)
 
-    # Perform classification
-    
+
+
 
 
 
