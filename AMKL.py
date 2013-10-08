Correct the method to calculate mean average precision

AMKL.py
@@ -4,7 +4,7 @@ from scipy.io import loadmat
 import math
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.metrics import average_precision_score
+import Utility as util
 
 def constructBaseKernels(kernel_type, kernel_params, D2):
 
@@ -226,7 +226,8 @@ def runAMKL(distances, labels, trainingIndiceList, testingIndiceList):
         testKernels = returnKernel(coefficients, testingBaseKernels, test_addKernel)
 
         testScores = SVMmodel.decision_function(testKernels)
-        AP = average_precision_score(testingLabels, testScores)
+        AP = util.averagePrecision(testScores, testingLabels)
+
         aps.append(AP)
 
     return aps

AdaptiveSVM.py
@@ -2,7 +2,7 @@ from scipy.io import loadmat
 import math
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.metrics import average_precision_score
+import Utility as util
 
 def constructBaseKernels(kernel_type, kernel_params, D2):
 
@@ -95,7 +95,8 @@ def runASVM(distances, labels, auxiliaryIndices, targetTrainingIndice, targetTes
         tempFinalTestScores = 1.0 / (1 + math.e **(-finalTestScores))
         finalTestScores = np.mean(tempFinalTestScores, axis = 0)
 
-        AP = average_precision_score(testingLabels, finalTestScores)
+        AP = util.averagePrecision(finalTestScores, testingLabels)
+
         aps.append(AP)
 
     return aps

FR.py
@@ -4,7 +4,7 @@ from scipy.io import loadmat
 import math
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.metrics import average_precision_score
+import Utility as util
 
 def constructBaseKernels(kernel_type, kernel_params, D2):
 
@@ -84,7 +84,7 @@ def runSVM_AT(distances, labels, auxiliaryIndices, targetTrainingIndice, targetT
         tempFinalTestScores = 1.0 / (1 + math.e **(-finalTestScores))
         finalTestScores = np.mean(tempFinalTestScores, axis = 0)
 
-        AP = average_precision_score(testingLabels, finalTestScores)
+        AP = util.averagePrecision(finalTestScores, testingLabels)
         aps.append(AP)
 
     return aps

MKL.py
@@ -3,7 +3,7 @@ from scipy.io import loadmat
 import math
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.metrics import average_precision_score
+import Utility as util
 
 def constructBaseKernels(kernel_type, kernel_params, D2):
 
@@ -81,7 +81,8 @@ def runMKL(distances, labels, trainingIndiceList, testingIndiceList):
         testKernels = returnKernel(coefficients, testingBaseKernels)
 
         testScores = SVMModel.decision_function(testKernels)
-        AP = average_precision_score(testingLabels, testScores)
+        AP = util.averagePrecision(testScores, testingLabels)
+
         aps.append(AP)
 
     return aps

test.py
@@ -1,9 +1,9 @@
-from sklearn.metrics import average_precision_score
 import numpy as np
+import Utility as util
 
-score = np.array([0.8, 0.4, 0.35, 0.1])
-label = np.array([1, 0 , 1, 0])
+score = np.array([0, 1, -1, 0, 0])
+label = np.array([-1, 1, 1, -1, -1])
 
-ap = average_precision_score(label, score)
+ap = util.averagePrecision(score, label)
 
 print ap